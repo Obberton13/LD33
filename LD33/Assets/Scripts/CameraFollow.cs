1a98301cad47c1cf4d845bf267bc301c06619917
Obberton13 <obberton130@gmail.com>
2015-08-21 23:52:37 -0500

    Initial Commit - Basic Movement & ideadoc

---
+++ LD33/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	private Transform _toFollow;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = _toFollow.transform.position;
+		pos.y = 0;
+		pos.z = -10;
+		transform.position = pos;
+	}
+}
---
+++ LD33/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField]
+	private float _moveSpeed;
+	private Rigidbody2D _rb2d;
+	// Use this for initialization
+	void Start () {
+		_rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 velocity = _rb2d.velocity;
+		velocity.x = Input.GetAxisRaw("Horizontal")*_moveSpeed;
+		velocity.y = Input.GetAxisRaw("Vertical")*_moveSpeed;
+		_rb2d.velocity = velocity;
+	}
+}
